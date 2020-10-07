cbed57b040ed3ede02d99076cfb3792e72634852
Snex Thadeus <lwande94@gmail.com>
2020-10-07 23:48:16 +0300

    First Commit

---
+++ src/main/java/com/teddy/login/CustomUserService.java
@@ -0,0 +1,24 @@
+package com.teddy.login;
+
+import com.teddy.login.CustomUser;
+import com.teddy.login.User;
+import com.teddy.login.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUser(user);
+    }
+}
